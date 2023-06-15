ead4d24ac4a27e5b746c066a09964d83507895ad
Manuel Rombe <manuelfungatejr@gmail.com>
2023-06-15 15:01:59 -0700

    my puts example

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Wher the whole program runs
+ *
+ * Return: is always 0
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
