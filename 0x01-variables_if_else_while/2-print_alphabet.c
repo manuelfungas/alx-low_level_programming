9d9d1b9cde029683d2fb9a75ade1124152affa15
Manuel Rombe <manuelfungatejr@gmail.com>
2023-06-16 16:55:10 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char alfa;
+
+	for (alfa = 'a'; alfa <= 'z'; alfa++)
+	{
+		putchar(alfa);
+	}
+	return (0);
+}
