a8b195e4cf84196113ed8f0988aafd35c505d99e
Manuel Rombe <manuelfungatejr@gmail.com>
2023-06-20 09:03:23 -0700

    print the alphabet

---
+++ 1-alphabet.c
@@ -0,0 +1,18 @@
+#include "_putchar.h"
+
+/**
+ * print_alphabet - prints the alphabet in lower-case
+ * @alfa - is the control variable
+ *
+ * Return: Void
+ */
+void print_alphabet(void)
+{
+	char alfa;
+
+	for (alfa = 'a'; alfa <= z; alfa++)
+	{
+		_putchar(alfa);
+	}
+	_putchar('\n');
+}
