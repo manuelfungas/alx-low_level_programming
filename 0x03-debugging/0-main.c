ae0d600ef4be2e287575acbd3ea676d13dd0af0b
Manuel Rombe <manuelfungatejr@gmail.com>
2023-06-22 10:37:06 -0700

    debugging the _is_neg_or_pos function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - to print if a number is negative or  positive
+ *
+ * return: returns 0 
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef POSITIVE_OR_NEGATIVE
+#define POSITIVE_OR_NEGATIVE
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* POSITIVE_OR_NEGATIVE */
