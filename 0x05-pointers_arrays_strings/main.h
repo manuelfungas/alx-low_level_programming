9e1375603af18a4f50adaf1e952df65b95acb4ad
Manuel Rombe <manuelfungatejunior@gmail.com>
2023-06-26 21:32:15 +0200

    changing the value back

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef RESET_H
+#define RESET_H
+
+void reset_to_98(int *n);
+
+#endif /* RESET_H */
+
+
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+
+int _putchar(char c);
+
+#endif /* _PUTCHAR_H */
+
