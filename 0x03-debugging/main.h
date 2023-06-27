4c4b271303e280f8034cae4e41d2b81ae2675a84
Manuel Rombe <manuelfungatejunior@gmail.com>
2023-06-27 10:38:37 +0200

    print the largest

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,3 +6,11 @@
 void positive_or_negative(int i);
 
 #endif /* POSITIVE_OR_NEGATIVE */
+
+
+#ifndef LARGEST_NUMBER_H
+#define LARGEST_NUMBER_H
+
+int largest_number(int a, int b, int c);
+
+#endif /* LARGEST_NUMBER_H */
