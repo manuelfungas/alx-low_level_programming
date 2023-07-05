dca442a2d8bd3bdfaf92f278e4c924df543585ad
Manuel Rombe <manuelfungatejr@gmail.com>
2023-07-05 23:28:45 +0200

    print reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string recursively
+ *
+ * @s: the string
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
