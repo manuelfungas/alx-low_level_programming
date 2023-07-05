afd79bb33f7e32d4c399939779df36ec32bf8dcb
Manuel Rombe <manuelfungatejr@gmail.com>
2023-07-06 00:09:48 +0200

    factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+/**
+ * factorial - calculates factorial
+ *
+ * @n: factorial to be found
+ *
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
