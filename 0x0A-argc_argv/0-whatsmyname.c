128e104bf5dbce0402b4aba123000a8a678a5ade
Manuel Rombe <manuelfungatejr@gmail.com>
2023-07-07 21:54:15 +0200

    let's go args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array of strings
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
