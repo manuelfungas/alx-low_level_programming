d09dade4c7c6b23fde7eb59358100c0b7081f29e
Manuel Rombe <manuelfungatejr@gmail.com>
2023-07-07 21:46:36 +0200

    let's go args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the executable file
+ * @argc: counter
+ * @argv: array of strings
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Learning main function arguments
