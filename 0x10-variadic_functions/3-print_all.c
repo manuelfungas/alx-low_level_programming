f395104f463a840b805dc4768beb26e9aa57b2f1
Manuel Rombe <manuelfungatejr@gmail.com>
2023-07-24 12:46:53 +0200

    to handle everythig is fun

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -20,19 +20,19 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%s%c", sep, va_arg(args, int));
+					printf("%s%c", separator, va_arg(args, int));
 					break;
 				case 'i':
-					printf("%s%d", sep, va_arg(args, int));
+					printf("%s%d", separator, va_arg(args, int));
 					break;
 				case 'f':
-					printf("%s%f", sep, va_arg(args, double));
+					printf("%s%f", separator, va_arg(args, double));
 					break;
 				case 's':
 					str = va_arg(args, char *);
 					if (!str)
 						str = "(nil)";
-					printf("%s%s", sep, str);
+					printf("%s%s", separator, str);
 					break;
 				default:
 					i++;
