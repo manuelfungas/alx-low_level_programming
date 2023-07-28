531837d930cbac1ecb137f143170377e6f530a2b
Manuel Rombe <manuelfungatejr@gmail.com>
2023-07-28 11:33:48 +0200

    Number of nodes of the number of data

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * list_len - gets the number of nodes
+ *
+ * @h: pointer to the head
+ *
+ * Return: number of nodes
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 1;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
+
+	return (count);
+}
