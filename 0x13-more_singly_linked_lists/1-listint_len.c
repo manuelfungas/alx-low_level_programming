cf4213d7b6210417bc5267e11764a9292a21d1bc
Manuel Rombe <manuelfungatejr@gmail.com>
2023-08-01 16:13:12 +0200

    number of elements of a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * list_len - gets the number of nodes
+ *
+ * @h: pointer to the head
+ *
+ * Return: number of nodes
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 1;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
