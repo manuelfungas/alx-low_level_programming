b14eae23211d3ed152c900b76540afac9bd5eb4c
Manuel R. Fungate Jr <manuelfungatejr@gmail.com>
2024-02-08 03:13:37 -0800

    Functin to transform binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,58 @@
+/**
+ * _strlen - string length
+ *
+ * @s: the string argument
+ * Return: len
+ */
+int _strlen(const char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		len = len + 1;
+		s = s + 1;
+	}
+
+	return (len);
+}
+/**
+ * binary_to_uint - converts binary to decimal
+ *
+ * @b: the binary argument
+ *
+ * Return: integer on success
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len;
+	unsigned int total, base;
+
+	len = _strlen(b);
+	total = 0;
+	base = 1;
+
+	if (!b)
+	{
+		return (0);
+	}
+
+	int i;
+
+	for (i = (len - 1); i >= 0; i--)
+	{
+		if (b[i] != '1' && b[i] != '0')
+		{
+			return (0);
+		}
+		if (b[i] & 1)
+		{
+			total = total + base;
+		}
+		base = base * 2;
+	}
+
+	return (total);
+}
