a0a5c1cac2ff1d98cb881652dbacc2b272fdf72f
Manuel R. Fungate Jr <manuelfungatejunior@gmail.com>
2024-02-12 03:40:03 -0800

    Read File

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * read_textfile - Reads text file
+ *
+ * @filename: pointer to name of file
+ * @letters: number of letters
+ *
+ * Return: 0 on success
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, bytes_read, bytes_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	bytes_read = read(fd, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (fd == -1 || bytes_read == -1 || bytes_written == -1
+		|| bytes_written != bytes_read)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (bytes_written);
+}
